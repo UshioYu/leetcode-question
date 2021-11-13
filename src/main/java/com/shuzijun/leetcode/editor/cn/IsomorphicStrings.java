47b5baccd161a6dab20bff38f9539a6b850b042a
yujian <yujian@ysten.com>
2021-11-13 17:24:53 +0800

    commit

---
+++ src/main/java/com/shuzijun/leetcode/editor/cn/IsomorphicStrings.java
@@ -0,0 +1,67 @@
+//给定两个字符串 s 和 t，判断它们是否是同构的。 
+//
+// 如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。 
+//
+// 每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入：s = "egg", t = "add"
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "foo", t = "bar"
+//输出：false 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "paper", t = "title"
+//输出：true 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 可以假设 s 和 t 长度相同。 
+// 
+// Related Topics 哈希表 字符串 👍 401 👎 0
+
+package com.shuzijun.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+    public static void main(String[] args) {
+        Solution solution = new IsomorphicStrings().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isIsomorphic(String s, String t) {
+            Map<Character,Character> s2t = new HashMap<>();
+            Map<Character,Character> t2s = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                char x = s.charAt(i);
+                char y = t.charAt(i);
+                if ((s2t.containsKey(x) && s2t.get(x) != y) ||
+                        (t2s.containsKey(y) && t2s.get(y) != x)) {
+                    return false;
+                }
+                s2t.put(x,y);
+                t2s.put(y,x);
+            }
+            return true;
+        }
+    }
+   //leetcode submit region end(Prohibit modification and deletion)
+
+}
